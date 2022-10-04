3ec26d350f1b2703ca5dd4c25a5fd873b2121079
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2022-10-04 20:37:56 +0530

    New react app created

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <>
+        <section className='min-h-screen grid content-center bg-black'>
+            <h1 className="text-8xl font-bold text-red-400 text-center">RaOG</h1>
+            <p className="text-2xl text-center text-red-400">Coming Soon</p>
+        </section>
+    </>
+  )
+}
